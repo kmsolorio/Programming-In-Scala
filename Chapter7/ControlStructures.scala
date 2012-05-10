4c42d98e80aea0cdaf97e96bb770535fed041402
kmsolorio <kevin@detone8.com>
2012-05-10 19:59:01 -0400

    for statements

--- Chapter7/ControlStructures.scala
+++ Chapter7/ControlStructures.scala
@@ -47,6 +47,37 @@ def gcdLoop(x: Long, y: Long): Long = {
 
 var line = ""
 do {
-  line = readline()
+  line = "Hello"
   println("Read: " + line )
-} while (link != "")
+} while (line != "Hello")
+
+
+//  For expressions can be used for many things in Scala.  The simplest way to
+//  use a for expression to to iterate over a collection.
+
+val filesHere = (new java.io.File(".")).listFiles
+for (file <- filesHere)
+  println(file)
+
+//  for can be used with any type of collection, including ranges.
+//  1 to 4 would give you 1, 2, 3, 4
+//  1 until 4 would give you 1, 2, 3
+
+// Filters can be created by adding an if clause within the parentheses of the for
+// for (i <- 1 to 4 if i == 3)
+
+// Scala can return a value from the iteration by prepending the body with the keyword
+// yield
+
+def evenNumbers = {
+  for (n <- 1 to 100 if n % 2 == 0) yield {
+    n
+  }
+}
+
+println(evenNumbers)
+
+
+// Scala has conventional try-catch-finally for exceptions
+
+
