703949aa1398750474eb1b7da16cd62d58024eea
kmsolorio <kevin@detone8.com>
2012-05-22 22:57:20 -0400

    begin chapter 9

---
+++ Chapter9/ReducingCodeDuplication.scala
@@ -0,0 +1,11 @@
+// Original FileMatcher Code.
+
+object FileMatcher {
+
+  private def filesHere = (new java.io.File(".")).listFiles
+
+  def filesEnding(query: String) = {
+    for (file <- filesHere; if file.getName.endsWith(query))
+      yield file
+  }
+}
